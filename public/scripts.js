9cb95f32b0307047d1d9da78c0f368956384a70a
guyNattawat <54878374+guyNattawat@users.noreply.github.com>
2023-02-26 00:35:57 +0700

    Update V0.2.1

--- public/scripts.js
+++ public/scripts.js
@@ -3,60 +3,28 @@ function navbar(){
     menu.classList.toggle('hidden');
 }
 
-const items = [
-    {
-        position: 0,
-        el: document.getElementById('carousel-item-1')
-    },
-    {
-        position: 1,
-        el: document.getElementById('carousel-item-2')
-    },
-    {
-        position: 2,
-        el: document.getElementById('carousel-item-3')
-    },
-    {
-        position: 3,
-        el: document.getElementById('carousel-item-4')
-    },
-];
+let currentSlide = 1;
 
-const options = {
-    defaultPosition: 1,
-    interval: 3000,
-    
-    indicators: {
-        activeClasses: 'bg-white dark:bg-gray-800',
-        inactiveClasses: 'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
-        items: [
-            {
-                position: 0,
-                el: document.getElementById('carousel-indicator-1')
-            },
-            {
-                position: 1,
-                el: document.getElementById('carousel-indicator-2')
-            },
-            {
-                position: 2,
-                el: document.getElementById('carousel-indicator-3')
-            },
-            {
-                position: 3,
-                el: document.getElementById('carousel-indicator-4')
-            },
-        ]
-    },
-    
-    // callback functions
-    onNext: () => {
-        console.log('next slider item is shown');
-    },
-    onPrev: ( ) => {
-        console.log('previous slider item is shown');
-    },
-    onChange: ( ) => {
-        console.log('new slider item has been shown');
+function showSlides(n){
+    let i = 0;
+    let slides = document.querySelectorAll('#slideShow');
+    let nexSlide = 0;
+    if(n == 1){
+        nexSlide = (1 + currentSlide) % (slides.length+1)
+        if(nexSlide < 1){
+            nexSlide = nexSlide + 1;
+        }
+    }if(n < 1){
+        nexSlide = (currentSlide - 1)
+        if(nexSlide < 1){
+            nexSlide = slides.length
+        }
     }
-};
+    currentSlide = nexSlide;
+    for(let i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    slides[nexSlide - 1].style.display = 'block';
+   
+    
+}
